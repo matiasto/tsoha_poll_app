246eddba7faed4ade59b565f0c61e9222312e0bf
matiasto <matias.tolppanen@helsinki.fi>
2022-03-30 15:31:47 +0300

    feat: basic frame for homepage

---
+++ frontend/src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div class="home">
+        <h1>Home</h1>
+        <p>
+            Lorem Ipsum is - en muista enään.
+        </p>
+    </div>
+  );
+}
+
+export default Home;
