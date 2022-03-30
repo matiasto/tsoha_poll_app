f8d5a446dd0922a193cb963db956b3080798ebe2
matiasto <matias.tolppanen@helsinki.fi>
2022-03-30 15:55:36 +0300

    feat: State for metadata and input fields for it

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,11 +1,45 @@
 import { useState } from "react";
 
-function CreatePoll() {
-    return (
-        <div class="create_poll">
-            <h1>Create a New Poll</h1>
-            <div class="meta">
+const defaultMeta = {
+    poll_title: "",
+    poll_description: "",
+    credits_per_voter: 1
+};
 
+function CreatePoll() {
+    const [meta, setMeta] = useState(defaultMeta)
+
+    return (
+        <div className="create_poll">
+            <h1>Create a New Poll</h1>
+            <div className="meta">
+                <div className="poll_title">
+                    <h4>Poll Title</h4>
+                    <input
+                        type="text"
+                        id="poll_title"
+                        value={meta.poll_title}
+                    />
+                </div>
+                <div className="poll_description">
+                    <h4>Description</h4>
+                    <input
+                        type="text"
+                        id="poll_description"
+                        value={meta.poll_description}
+                        maxLength="300"
+                    />
+                </div>
+                <div className="credits_per_voter">
+                    <h4>Credits Per Voter</h4>
+                    <input
+                        type="number"
+                        id="creadits_per_voter"
+                        value={meta.credits_per_voter}
+                        min="1"
+                        max="250"
+                    />
+                </div>
             </div>
         </div>
     );
