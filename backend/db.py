9bf6b15b8680a3b7b12e77ac65fcc8b5beda2502
matiasto <matias.tolppanen@helsinki.fi>
2022-03-30 15:02:32 +0300

    database connection established

---
+++ backend/db.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
+from app import app
+
+load_dotenv(find_dotenv())
+
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+print(db.session.execute("SELECT 1").fetchone())
