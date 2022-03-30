8bd6bda2e73ce0baeb88d99a5efe5b932e40d33e
matiasto <matias.tolppanen@helsinki.fi>
2022-03-30 15:42:58 +0300

    feat: init create page for poll app

---
+++ frontend/src/Create.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function CreatePoll() {
+    return (
+        <div class="create_poll">
+            <h1>Create a New Poll</h1>
+            <div class="meta">
+
+            </div>
+        </div>
+    );
+}
+
+export default CreatePoll;
