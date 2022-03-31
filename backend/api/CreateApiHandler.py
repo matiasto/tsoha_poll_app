e62b94fdb6e9fd6403508c58cdf7bfd52bac0e3a
matiasto <matias.tolppanen@helsinki.fi>
2022-03-31 10:27:51 +0300

    feat[db]:poll submission to db

--- backend/api/CreateApiHandler.py
+++ backend/api/CreateApiHandler.py
@@ -1,15 +1,42 @@
 from flask import request
 from flask_restful import Resource
-from backend.db import db
+from db import db
 
 
 class CreateApiHandler(Resource):
     def post(self):
         package = request.json['body']
-        for i in package['poll']:
-            print(i)
-        print(package['meta']['poll_title'])
-        print(package['meta']['poll_description'])
-        print(package['meta']['credits_per_voter'])
-        
+        meta = package['meta']
+        poll_title = meta["poll_title"]
+        poll_description = meta["poll_description"]
+        poll_credits = meta["credits_per_voter"]
 
+        sql = """INSERT INTO polls (title, description, credits, created_at) 
+                    VALUES (:title, :description, :credits, NOW()) RETURNING poll_id"""
+
+        result = db.session.execute(
+            sql, {"title": poll_title,
+                  "description": poll_description,
+                  "credits": poll_credits})
+
+        poll_id = result.fetchone()[0]
+        questions = package["poll"]
+        for question in questions:
+            header = question["header"]
+            description = question["description"]
+            if header != "":
+                if description == "":
+                    description = None
+
+                sql = """INSERT INTO questions (poll_id, header, description) 
+                            VALUES (:poll_id, :header, :description)"""
+
+                db.session.execute(sql, {"poll_id": poll_id,
+                                         "header": header,
+                                         "description": description})
+
+        db.session.commit()
+        return {
+            "status": "SUCCESS",
+            "message": "Poll submitted!"
+        }
---
+++ backend/schema.sql
@@ -0,0 +1,34 @@
+drop table if exists answers;
+drop table if exists questions;
+drop table if exists polls;
+
+create table polls (
+	poll_id integer generated always as identity,
+	title text,
+	description text,
+	credits integer,
+	created_at timestamp,
+	primary key(poll_id)
+);
+
+create table questions (
+	question_id integer generated always as identity,
+	poll_id integer,
+	header text,
+	description text,
+	primary key(question_id),
+	constraint fk_poll
+    	foreign key (poll_id) 
+	  		references polls(poll_id)
+);
+
+create table answers (
+    answer_id integer generated always as identity,
+    question_id integer,
+    votes integer ,
+    sent_at timestamp,
+    primary key(answer_id),
+	constraint fk_question
+    	foreign key (question_id) 
+	  		references questions(question_id)
+);
