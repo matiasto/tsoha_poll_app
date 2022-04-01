f3fcaf2432279a09d296e7c0c1af6a6a76fa42d1
matiasto <matias.tolppanen@helsinki.fi>
2022-04-01 17:12:56 +0300

    fix[attempt]: setting port

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__, static_folder="client/build", static_url_path="/")
+
+p = int(os.environ.get("PORT", 5000))
+app.run(debug=True, port=p, host='0.0.0.0')
 
 from flask_restful import Api
 from api.PollsApi import PollsApi
