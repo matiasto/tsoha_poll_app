dc73f26cc2ef75f445d8caeddad1450cb581dc48
matiasto <matias.tolppanen@helsinki.fi>
2022-04-01 17:47:19 +0300

    config[deployment]: heroku

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ import os
 
 app = Flask(__name__, static_folder="client/build", static_url_path="/")
 
-p = int(os.environ.get("PORT", 5000))
-app.run(debug=True, port=p, host='0.0.0.0')
-
 from flask_restful import Api
 from api.PollsApi import PollsApi
 from api.PollApi import PollApi
 
 api = Api(app)
 
-@app.route('/')
+@app.route("/")
 def serve():
     return send_from_directory(app.static_folder, "index.html")
 
--- db.py
+++ db.py
@@ -1,9 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
-from dotenv import load_dotenv, find_dotenv
 from app import app
-
-load_dotenv(find_dotenv())
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
