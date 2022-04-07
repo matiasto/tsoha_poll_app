386cd27152a15622b1dc36968cc932c9bbc48fda
matiasto <matias.tolppanen@helsinki.fi>
2022-04-07 22:17:48 +0300

    feat[security]: input filtering

--- api/PollApi.py
+++ api/PollApi.py
@@ -1,5 +1,5 @@
 import json
-from flask import jsonify, request
+from flask import request
 from flask_restful import Resource
 from db import db
 from .to_json_format import FormatterTool
@@ -16,17 +16,40 @@ class PollApi(Resource):
 
     def post(self, poll_id):
         data = json.loads(request.data)
+        credits = data.pop()["credits"]
+        check = 0
+        for item in data:
+            question_id = item["id"]
+            if not isinstance(question_id, int):
+                return {
+                    "status": 403,
+                    "message": "forbidden question id!"
+                }
+            votes = item["votes"]
+            check += votes ** 2
+        if check > credits ** 2:
+            return {
+                "status": 403,
+                "message": "credits overflow!"
+            }
         for item in data:
             question_id = item["id"]
             votes = item["votes"]
-
             sql = """INSERT INTO answers (question_id, votes, sent_at) 
                             VALUES (:question_id, :votes, NOW())"""
             result = db.session.execute(sql, {"question_id": question_id,
                                               "votes": votes})
         db.session.commit()
+        return {
+            "status": 200,
+            "message": "Succesfully submitted"
+        }
 
     def delete(self, poll_id):
         sql = "UPDATE polls SET visible=0 WHERE poll_id=:id"
-        db.session.execute(sql, {"id":poll_id})
+        db.session.execute(sql, {"id": poll_id})
         db.session.commit()
+        return {
+            "status": 200,
+            "message": "Succesfully deleted"
+        }
--- api/PollsApi.py
+++ api/PollsApi.py
@@ -3,6 +3,11 @@ from flask_restful import Resource
 from .to_json_format import FormatterTool
 from db import db
 
+def bail_out(poll_id):
+    sql = "UPDATE polls SET visible=0 WHERE poll_id=:id"
+    db.session.execute(sql, {"id": poll_id})
+    db.session.commit()
+
 
 class PollsApi(Resource):
     def get(self):
@@ -18,25 +23,52 @@ class PollsApi(Resource):
         poll_title = meta["poll_title"]
         poll_description = meta["poll_description"]
         poll_credits = meta["credits_per_voter"]
+        if len(poll_title) > 100 or poll_title == "":
+            return {
+                "status": 403,
+                "Message": "Error in title!"
+            }
+        if len(poll_description) > 300:
+            return {
+                "status": 403,
+                "message": "Description is too long!"
+            }
+        if poll_credits > 250 or poll_credits < 0:
+            return {
+                "status": 403,
+                "message": "Error in credits!"
+            }
 
-        sql = """INSERT INTO polls (title, description, credits, visible, created_at) 
+        sql = """INSERT INTO polls (title, description, credits, visible, created_at)
                     VALUES (:title, :description, :credits, 1, NOW()) RETURNING poll_id"""
 
         result = db.session.execute(
             sql, {"title": poll_title,
                   "description": poll_description,
-                  "credits": poll_credits})
+                  "credits": poll_credits}
+        )
 
         poll_id = result.fetchone()[0]
         questions = request.json['poll']
         for question in questions:
             header = question["header"]
             description = question["description"]
-            if header != "":
-                if description == "":
-                    description = None
+            if len(header) > 100 or header == "":
+                bail_out(poll_id)
+                return {
+                    "status": 403,
+                    "Message": "Error in statement header!"
+                }
+            if len(description) > 300:
+                bail_out(poll_id)
+                return {
+                    "status": 403,
+                    "Message": "Error in statement description!"
+                }
+            if description == "":
+                description = None
 
-                sql = """INSERT INTO questions (poll_id, header, description) 
+                sql="""INSERT INTO questions (poll_id, header, description)
                             VALUES (:poll_id, :header, :description)"""
 
                 db.session.execute(sql, {"poll_id": poll_id,
@@ -45,6 +77,6 @@ class PollsApi(Resource):
 
         db.session.commit()
         return {
-            "status": "SUCCESS",
+            "status": 200,
             "message": "Poll submitted!"
         }
