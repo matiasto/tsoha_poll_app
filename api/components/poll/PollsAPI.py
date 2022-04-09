54de6fc414dfebde84fc828ce84a01bfc948533b
matiasto <matias.tolppanen@helsinki.fi>
2022-04-09 21:39:07 +0300

    feat: adjustment to new db/service methods

--- api/components/poll/PollsAPI.py
+++ api/components/poll/PollsAPI.py
@@ -3,16 +3,14 @@ from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 from ...services.to_json_format import FormatterTool
 from ...services.validate import Validate
-from api.db.db import db
+from ...db.polls import Polls
 
 
 class PollsAPI(Resource):
     method_decorators = [jwt_required()]
 
     def get(self):
-        sql = "SELECT poll_id, title, description, credits, created_at FROM polls WHERE visible=1 ORDER BY poll_id DESC"
-        result = db.session.execute(sql)
-        polls = result.fetchall()
+        polls = Polls.get_polls()
         headers = ["poll_id", "title", "description", "credits", "created_at"]
         data = FormatterTool.to_json(headers, polls, to_json=True)
         return jsonify(data)
@@ -26,31 +24,9 @@ class PollsAPI(Resource):
             poll_title, poll_description, poll_credits)
         if code == 403:
             return message, code
-
-        sql = """INSERT INTO polls (title, description, credits, visible)
-                    VALUES (:title, :description, :credits, 1) RETURNING poll_id"""
-
-        result = db.session.execute(
-            sql, {"title": poll_title,
-                  "description": poll_description,
-                  "credits": poll_credits}
-        )
-
-        poll_id = result.fetchone()[0]
         statements = request.json['poll']
         message, code = Validate.statements(statements)
         if code == 403:
             return message, code
-        for statement in statements:
-            header = statement["header"]
-            description = statement["description"]
-
-            sql = """INSERT INTO questions (poll_id, header, description)
-                        VALUES (:poll_id, :header, :description)"""
-
-            db.session.execute(sql, {"poll_id": poll_id,
-                                     "header": header,
-                                     "description": description})
-
-        db.session.commit()
+        Polls.post_poll(poll_title, poll_description, poll_credits, statements)
         return {"message": "Poll submitted!"}, 200
