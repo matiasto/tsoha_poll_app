cbea23cd89f252f0383ed7e494f5e795fb042e84
matiasto <matias.tolppanen@helsinki.fi>
2022-04-09 21:33:20 +0300

    feat[db]: new db methods

---
+++ api/db/polls.py
@@ -0,0 +1,22 @@
+from ..services.fetch_queries import FetchQuery
+from .db import db
+
+
+class Polls:
+    @staticmethod
+    def post_poll(poll_title: str, poll_description: str, poll_credits: str, statements: list):
+        sql = FetchQuery.get_sql_query("post_poll_meta")
+        result = db.session.execute(
+            sql, {"title": poll_title,
+                  "description": poll_description,
+                  "credits": poll_credits}
+        )
+        poll_id = result.fetchone()[0]
+        sql = FetchQuery.get_sql_query("post_poll_statement")
+        for statement in statements:
+            header = statement["header"]
+            description = statement["description"]
+            db.session.execute(sql, {"poll_id": poll_id,
+                                     "header": header,
+                                     "description": description})
+        db.session.commit()
